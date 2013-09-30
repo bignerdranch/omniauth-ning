62f8cd2193702dcd93c06675b8a5f4845019d601
Don Pottinger <don.pottinger@gmail.com>
2013-09-30 17:24:16 -0400

    Updates request and callback phase.

--- lib/omniauth/strategies/ning.rb
+++ lib/omniauth/strategies/ning.rb
@@ -7,15 +7,22 @@ module OmniAuth
 
         DEFAULT_ENDPOINT = 'https://external.ningapis.com/xn/rest/YRGUniverse/1.0'
         option :name, "ning"
-        option :fields, :email
+        option :oauth_token
 
         # This is where you pass the options you would pass when
         # initializing your consumer from the OAuth gem.
         option :client_options, {
           site:           DEFAULT_ENDPOINT,
-          authorize_url:  "/Token",
           token_url:      "/Token"
         }
+
+      def request_phase
+        if options.oauth_token.present?
+          super
+        else
+          redirect '/ning_sessions/new'
+        end
+      end
 
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
@@ -26,9 +33,10 @@ module OmniAuth
 
       info do
         {
-          author: raw_info['entry']['author'],
-          oauth_token: raw_info['entry']['oauthToken'],
-          oauth_token_secret: raw_info['entry']['oauthTokenSecret']
+          author: params['entry']['author'],
+          oauth_consumer_key: params['entry']['oauth_consumer_key'],
+          oauth_token: params['entry']['oauth_token'],
+          oauth_token_secret: params['entry']['oauth_token_secret']
         }
       end
 
